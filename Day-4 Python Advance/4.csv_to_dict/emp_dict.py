9be4f5338257637949e2a3dfed109e14daca51d6
VIGNASH <83445615+vignashmahendran@users.noreply.github.com>
2023-07-06 17:52:18 +0530

    Day 4 completed

--- Day-4 Python Advance/4.csv_to_dict/emp_dict.py
+++ Day-4 Python Advance/4.csv_to_dict/emp_dict.py
@@ -1,13 +1,13 @@
 import csv
-import json
-employee_list=[]
+employee_list={}
 keys=['EMPLOYEE_ID','FIRST_NAME','LAST_NAME','EMAIL','PHONE_NUMBER','HIRE_DATE','JOB_ID','SALARY','COMMISSION_PCT','MANAGER_ID','DEPARTMENT_ID']
 with open("./Data_set/test.csv",'r') as data:
     dict_1=csv.DictReader(data)
-
-    employee_list=list(dict_1)
-
-search=int(input("Enter Employee id to search  :  "))-1
+    for row in dict_1:
+       
+        employee_list[row['EMPLOYEE_ID']]=row
+    
+search=input("Enter Employee id to search  :  ")
 emp_detail=employee_list[search]
 for i in emp_detail:
     print(i +" : "+ emp_detail [i])
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 1.Python File Operation
 ## 2.Python Basic Logging and CSV to JSON
 ## 3.Pandas Framework
+## 4.Python Advance

f6c3a527b1d1d2399aa4e0eda76c869af797e655
VIGNASH <83445615+vignashmahendran@users.noreply.github.com>
2023-07-07 17:04:59 +0530

    Day 5 completed update

--- Day-5 API/rest_api.py
+++ Day-5 API/rest_api.py
@@ -1,12 +1,17 @@
 import requests
 import json
-import pandas
-hero_id=pandas.read_html("https://www.superheroapi.com/ids.html")[0]
+import pandas as pd
+hero_id=pd.read_html("https://www.superheroapi.com/ids.html")[0]
 print(" "*20 +"----Get all SuperHeroes and Villians data from all universes----")
 print(hero_id.iloc[:49,1:])
 search_id=str(int(input("Enter Id of the character:"))+1)
 response=requests.get(f"https://akabab.github.io/superhero-api/api/id/{search_id}.json")
 details=response.json()
+response=response.json()
+with open('text.json','w') as e:
+    json.dump(response,e,indent=4)
+r=pd.read_json('text.json')
+r.to_csv('file.csv')
 print(" "*20 +"---------------"+ details["name"] +"---------------")
 
 print(" "*25 +"---------"+ "powerstats"+"---------")
